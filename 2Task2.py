c260e60a99b7293e329af2bbe06b8a169cb48866
Your Name <olexandrkorchak@gmail.com>
2017-03-07 13:40:31 +0200

    task2 update @elpiankova

--- 2Task2.py
+++ 2Task2.py
@@ -1,24 +1,23 @@
-s = "We are not what we should be! \nWe are not what we need to be. \nBut at least we are not what we used to bе \n(Football Coach)"
+s = "We are not what we should be! \nWe are not what we need to be. \nBut at least we are not what we used to be \n(Football Coach)"
 print(s)
 s1 = s.split()
 print(s1)
 print(len(s1))
 
 s1.sort()
+
 for i in s1:
-    i=i.strip("!")
-    i=i.strip(",")
-    i=i.strip("(")
-    i=i.strip(")")
-    i=i.strip(".")
+    i=i.strip("!" + "," + "(" + ")"+ ".")
     print(i)
-    
+
 d = {}
 for i in s1:
+    i=i.strip("!" + "," + "(" + ")"+ ".")
     if i in d:
         d[i] = d[i] + 1
     else:
         d[i] = 1
 print(d)
+
 
 
