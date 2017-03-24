7c86d43b8bfe8d1ee3d05e1849bbe537a1a6c1fa
lex91chucky <olexandrkorchak@gmail.com>
2017-03-24 14:27:19 +0200

    Update Task24.py

--- Task24.py
+++ Task24.py
@@ -1,5 +1,5 @@
 def is_year_leap(year):
-    if ((year % 4 == 0) and (year % 100 > 0)) or (year % 400 == 0):
+    if ((year % 4 == 0) and (year % 100 > 0)) and (year % 400 != 0):
         a = True
     else:
         a = False
